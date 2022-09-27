1165115aec721708dcda97b06ea411b82f93d764
VictorEdM <thomasmannautor@gmail.com>
2022-09-27 11:15:37 -0300

    Post endpoint created and HTTP responses implemented

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 
 
 app = FastAPI()
@@ -10,19 +10,41 @@ async def root() -> dict:
 
 
 #Get/all
-@app.get("/todos", tags=["TODOS"])
-async def fetch_to_dos() -> dict:
-    return {"data": todos}
+@app.get("/todos", tags=["GET ALL TODOS"], status_code=status.HTTP_200_OK)
+async def fetch_todos() -> dict:
+    if todos:
+        return {"data": todos}
+    raise HTTPException(
+        detail="No todos on the list",
+        status_code=status.HTTP_404_NOT_FOUND
+    )
 
 
 #Get/id
-@app.get("/todos/{todo_id}", tags=["ONE TODO"])
-async def fetch_one_to_do(todo_id: int) -> dict:
-    return {"data": list(filter(lambda todo: todo.get("id") == todo_id, todos))[0]}
+@app.get("/todos/{todo_id}", tags=["GET ONE TODO"], status_code=status.HTTP_200_OK)
+async def fetch_one_todo(todo_id: int) -> dict:
+    todo: dict = list(filter(lambda t: t.get("id") == todo_id, todos))[0]
+    if todo:
+        return {"data": todo}
+    raise HTTPException(
+        detail=f"No todo with id {todo_id} on the list",
+        status_code=status.HTTP_404_NOT_FOUND
+    )
 
-#Post
-#Update
-#Delete
+
+#Post/to do
+@app.post("/todos", tags=["ADD TODO"], status_code=status.HTTP_201_CREATED)
+async def add_todo(todo: dict) -> dict:
+    if todo:
+        todos.append(todo)
+        return {"data": todo}
+    raise HTTPException(
+        detail="Todo not created. Body request must be empty",
+        status_code=status.HTTP_204_NO_CONTENT
+    )
+
+#Update/id/activity?
+#Delete/id
 
 
 todos = [
